f47c2f15dfb2606e510702c3913128f2fc2a0fbc
illust <illust0130@foxmail.com>
2022-03-07 07:28:01 +0800

    add scroll

---
+++ newbee-project-mock/src/components/ListScroll.vue
@@ -0,0 +1,16 @@
+<template>
+  <div ref="wrapper" class="scroll-wrapper">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+import BScroll from "better-scroll"
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
