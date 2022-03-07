e4c39a32505810ef7dc75a413f54cc8b56a013fa
svenwong <illust0130@foxmail.com>
2022-03-07 17:21:24 +0800

    add cart

--- newbee-project-mock/src/views/Cart.vue
+++ newbee-project-mock/src/views/Cart.vue
@@ -1,7 +1,17 @@
 <!--  -->
 <template>
-  <div>
-
+  <div class="cart">
+    <div class="loader">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
   </div>
 </template>
 
@@ -15,6 +25,66 @@ export default {
 }
 </script>
 
-<style  scoped>
+<style  lang="less" scoped>
+.cart{
+  margin:0;
+  padding:0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #89179e;
+}
+.loader{
+  position: relative;
+  width: 250px;
+  height: 250px;
+  transform-style: preserve-3d;
+  transform: perspective(500px) rotateX(70deg);
+
+  span{
+    position:absolute;
+    display: block;
+    border: 4px solid rgb(255, 255, 255);
+    box-sizing: border-box;
+    border-radius: 50%;
+  }
+  span:nth-child(1){
+    top:0;
+    left:0;
+    right:0;
+    bottom:0;
+  }
+  span:nth-child(2){
+    top:10px;
+    left:10px;
+    right:10px;
+    bottom:10px;
+    border: 4px solid rgb(231, 87, 87);
+
+  }
+  span:nth-child(3){
+    top:20px;
+    left:20px;
+    right:20px;
+    bottom:20px;
+    border: 4px solid rgb(63, 60, 231);
+  }
+  span:nth-child(4){
+    top:30px;
+    left:30px;
+    right:30px;
+    bottom:30px;
+    border: 4px solid rgb(0, 248, 95);
+  }
+  span:nth-child(5){
+    top:70px;
+    left:70px;
+    right:70px;
+    bottom:70px;
+    border: 6px solid rgb(252, 165, 3);
+    transform-style: preserve-3d;
+    transform: perspective(300px) rotateX(30deg);
+  }
+}
 
 </style>
