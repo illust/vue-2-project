65a514f35384efbc5b5fa778b68aad449bd90494
svenwong <illust0130@foxmail.com>
2022-03-14 17:54:07 +0800

    add works

--- newbee-project-mock/src/views/Login.vue
+++ newbee-project-mock/src/views/Login.vue
@@ -1,15 +1,25 @@
 <!--  -->
 <template>
-  <div>
+  <div class="login">
+    <s-header :name="type == 'login'?'登录':'注册'" :back="'/home'"></s-header>
+    <img src="//s.weituibao.com/1582958061265/mlogo.png" alt="" class="logo">
+    <div v-if="type == 'login'" class="login-body login">
+      
+    </div>
+    <div v-else class="login-body register">
 
+    </div>
   </div>
 </template>
 
 <script>
+import sHeader from '@/components/SimpleHeader'
 export default {
+  components: { sHeader },
   name: 'login',
   data () {
     return {
+      type: 'login'
     }
   }
 }
